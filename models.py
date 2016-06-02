e3561bd50e9754fb3bb7608f639b91303d2b7c6a
puri8467 <puri8467@gmail.com>
2016-06-02 15:19:04 +0900

    수정본 입니다.

--- models.py
+++ models.py
@@ -1,20 +1,8 @@
 from django.db import models
-from django.utils import timezone
-
-class Card(models.Model):
-    Color = models.CharField(max_length = 2, null = False)
-    Number = models.CharField(max_length = 5, null = False)
-    Is_match = models.BooleanField(default = False, null = False)
+#charfield() char
+#integerfield() number
+#
 
 class Player(models.Model):
-    Name = models.CharField(max_length = 20, null = False, unique = True)
-    Hand = models.ManyToManyField(Card)
-    Location = models.PositiveIntegerField()
-    Number = models.PositiveIntegerField()
-    Turn = models.BooleanField(default = False, null = False)
-	
-class Room(models.Model):
-    RoomName = models.CharField(max_length = 20, null = False, unique = True)
-    Num_of_player = models.PositiveIntegerField()
-    Is_start = models.BooleanField(default = False, null = False)
-    Deck = models.ForeignKey(Card)
+    player_number = 0
+    join_number = 0
--- views.py
+++ views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Player
+from .profile import Profile, Profile2
 
-def index(request):
-    return HttpResponse("Hello world")
+def post_main(request): 
+	return render(request, 'game/main.html', {})
+#main
 
-def post_main(request):
-    return render(request, 'game/main.html', {})
-def post_explain(request):
-    return render(request, 'game/explain.html', {})
-def post_game(request):
-    return render(request, 'game/game.html', {})
+def post_explain(request): 
+	return render(request, 'game/explain.html', {})
+#game explain
+
 def post_stay(request):
-    return render(request, 'game/stay.html', {})
-def post_login(request):
-    return render(request, 'game/login.html', {})
+	Player.player_number += 1
+	Player.join_number += 1
+
+	if(Player.join_number == 2):
+		text = {'player' : Player.player_number, 'join' :Player.join_number}
+		Player.join_number = 0
+
+		return render(request, 'game/game.html', text)
+		x
+	else:
+		text = {'player' : Player.player_number, 'join' :Player.join_number}
+	return render(request, 'game/stay.html', text)
+		
+# stay & give play num
+def post_game(request):
+    
+    return render(request, 'game/game.html', {})
+
+
 # Create your views here.
