e858466fe6e951fad0b907206efb64b779e69e41
melongl11 <melongl11@naver.com>
2016-06-03 15:25:17 +0900

    Stay.html 사용하도록

--- views.py
+++ views.py
@@ -1,32 +1,46 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.template import Context, loader
 from .models import Player
 from .profile import Profile, Profile2
 
-def post_main(request): 
-	return render(request, 'game/main.html', {})
+def post_main(request):
+	
+	Player.player_number += 1
+	
+	text = {'count' : Player.player_number , 'num' :Player.join_number} 
+	return render(request, 'game/main.html', text)
+
 #main
 
 def post_explain(request): 
 	return render(request, 'game/explain.html', {})
 #game explain
 
-def post_stay(request):
-	Player.player_number += 1
-	Player.join_number += 1
-
+def post_game(request):
+	Player.join_number+=1
+	waiting = False
+	Game_Start = False
+	
+	
 	if(Player.join_number == 2):
-		text = {'player' : Player.player_number, 'join' :Player.join_number}
+		waiting = True
+		Game_Start = True
+	else:
+		waiting = False
+
+	if  waiting == False:
+		tpl = loader.get_template('game/stay.html')
+		ctx = Context ({ })
+	elif waiting == True or Game_Start == True:
+		tpl = loader.get_template('game/game.html')
+		ctx = Context ({ })
 		Player.join_number = 0
 
-		return render(request, 'game/game.html', text)
-		x
-	else:
-		text = {'player' : Player.player_number, 'join' :Player.join_number}
-	return render(request, 'game/stay.html', text)
-		
+	return HttpResponse(tpl.render(ctx))
+	
 # stay & give play num
-def post_game(request):
+def post_stay(request):
     
     return render(request, 'game/game.html', {})
 
