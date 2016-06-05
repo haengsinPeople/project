e916cccaf8265533dc6d07916b661ed47f1d3f5c
puri8467 <puri8467@gmail.com>
2016-06-05 13:26:30 +0900

    수정 방

--- views.py
+++ views.py
@@ -1,48 +1,37 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Player, Room
 from django.template import Context, loader
-from .models import Player
-from .profile import Profile, Profile2
+from .data import Data1, Data2
 
 def post_main(request):
 	
-	Player.player_number += 1
-	
-	text = {'count' : Player.player_number , 'num' :Player.join_number} 
+	Player.player_number += 1 #플레이 시작하는 화면에서 플레이어를 올립시다 . 일단은 메인에서 확인용으로 
+	text = {'num': Player.player_number, 'b': Player.join_number} 
 	return render(request, 'game/main.html', text)
-
 #main
 
 def post_explain(request): 
-	return render(request, 'game/explain.html', {})
+	Player.player_number -= 1
+	return render(request, 'game/explain.html')
 #game explain
 
-def post_game(request):
-	Player.join_number+=1
-	waiting = False
-	Game_Start = False
+def post_game(request,num):
 	
-	
+	Player.join_number += 1
+	Room.room_number += 1
 	if(Player.join_number == 2):
-		waiting = True
-		Game_Start = True
-	else:
-		waiting = False
-
-	if  waiting == False:
-		tpl = loader.get_template('game/stay.html')
-		ctx = Context ({ })
-	elif waiting == True or Game_Start == True:
-		tpl = loader.get_template('game/game.html')
-		ctx = Context ({ })
-		Player.join_number = 0
-
-	return HttpResponse(tpl.render(ctx))
+		my_num = Player.player_number
+		text = {'a': my_num, 'b': Player.join_number, 'num':Room.room_number}	
 	
-# stay & give play num
-def post_stay(request):
-    
-    return render(request, 'game/game.html', {})
-
-
-# Create your views here.
+		return render(request, 'game/game.html', text)
+	
+	else:
+		Player.join_number=1
+		my_num = Player.player_number
+		text = {'a': my_num, 'b': Player.join_number, 'num':Room.room_number}	
+	
+		while(Player.join_number ==1):
+			pass
+		
+		return render(request, 'game/game.html', text)
